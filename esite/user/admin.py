e8657056f38c987543554e132349be635257f89e
dermrsimon <simon.prast.sp@gmail.com>
2019-07-07 18:30:56 +0200

    Add field username as uuid to User model

--- esite/user/admin.py
+++ esite/user/admin.py
@@ -10,7 +10,7 @@ class UserAdmin(ModelAdmin):
     add_to_settings_menu = False
     exclude_from_explorer = False
    
-    list_display = ('date_joined', 'title', 'first_name', 'last_name', 'email', 'telephone', 'address', 'zipCode', 'city', 'country', 'newsletter')
+    list_display = ('date_joined', 'username', 'title', 'first_name', 'last_name', 'email', 'telephone', 'address', 'zipCode', 'city', 'country', 'newsletter')
     search_fields = ('date_joined', 'title', 'first_name', 'last_name', 'email', 'telephone', 'address', 'zipCode', 'city', 'country', 'newsletter')
 
 modeladmin_register(UserAdmin)
