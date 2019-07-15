24b2b5fa96f16e40b46663865ed846e66898bad2
kleberbaum <kleberbaum@erebos.xyz>
2019-07-16 01:07:18 +0200

    Cleanup registration

--- esite/settings/production.py
+++ esite/settings/production.py
@@ -35,8 +35,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'noreply.pharmaziegasse@gmail.com' #os.environ['EMAIL_HOST_USER'] 
-EMAIL_HOST_PASSWORD = 'Pharmaziegasse-9020' #os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER'] 
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
 # BASE_URL required for notification emails
 BASE_URL = 'https://pharmaziegasse.at'
