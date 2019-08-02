4c67ede64339a62fcc12caf1ee2f0fda102030b7
kleberbaum <kleberbaum@erebos.xyz>
2019-08-02 11:19:36 +0200

    Add about field in imprint

--- esite/home/models.py
+++ esite/home/models.py
@@ -32,20 +32,21 @@ class Button(models.Model):
         on_delete=models.SET_NULL,
         related_name='+',
     )
-    
+
     panels = [
-      FieldPanel('button_title'),
-      FieldPanel('button_embed'),
-      FieldPanel('button_link'),
-      PageChooserPanel('button_page')
+        FieldPanel('button_title'),
+        FieldPanel('button_embed'),
+        FieldPanel('button_link'),
+        PageChooserPanel('button_page')
     ]
 
     def __str__(self):
-      return self.button_title
+        return self.button_title
+
 
 ## Header ##
 class Hero_SlideBlock(blocks.StructBlock):
-    slide_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="The bold header text at the frontpage slider") 
+    slide_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="The bold header text at the frontpage slider")
     slide_subhead = blocks.RichTextBlock(null=True, blank=False, help_text="The content of the frontpage slider element", features=['bold', 'italic', 'underline', 'strikethrough', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'ol', 'ul', 'hr', 'embed', 'link', 'superscript', 'subscript', 'document-link', 'image', 'code'], classname="full")
     slide_image = ImageChooserBlock(null=True, blank=False, help_text="Big, high resolution slider image")
     slide_button = SnippetChooserBlock(Button, null=True, blank=True, required=False, help_text="The button displayed at the frontpage slider")
@@ -53,7 +54,7 @@ class Hero_SlideBlock(blocks.StructBlock):
 class  _H_HeroBlock(blocks.ListBlock):
     def __init__(self, **kwargs):
         super(_H_HeroBlock, self).__init__(Hero_SlideBlock(), **kwargs)
-    
+
 
 ## Why Section ##
 class Why_CollumBlock(blocks.StructBlock):
@@ -128,7 +129,7 @@ class Services_ServiceBlock(blocks.StructBlock):
 class _S_ServicesBlock(blocks.StructBlock):
     services_background = ColorBlock(null=True, blank=False, help_text="Select background color that contrasts text")
     services_services = blocks.StreamBlock([
-      ('service', Services_ServiceBlock(null=True, blank=False, icon='doc-full'))
+        ('service', Services_ServiceBlock(null=True, blank=False, icon='doc-full'))
     ], null=True, blank=False)
     services_button = SnippetChooserBlock(Button, null=True, blank=True, required=False, help_text="Button displayed at services-block")
 
@@ -145,7 +146,7 @@ class _S_ReviewsBlock(blocks.StructBlock):
     reviews_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
     reviews_displayhead = blocks.BooleanBlock(null=True, blank=True, default=True, required=False, help_text="Whether or not to display the header")
     reviews_reviews = blocks.StreamBlock([
-      ('review', Reviews_ReviewBlock(null=True, blank=False))
+        ('review', Reviews_ReviewBlock(null=True, blank=False))
     ], null=True, blank=False)
 
 
@@ -163,7 +164,7 @@ class _S_FeaturesBlock(blocks.StructBlock):
     features_displaysubhead = blocks.BooleanBlock(null=True, blank=True, default=True, required=False, help_text="Whether or not to display the header")
     features_button = SnippetChooserBlock(Button, null=True, blank=True, required=False, help_text="Button displayed at features-block")
     features_features = blocks.StreamBlock([
-      ('feature', Features_FeatureBlock(null=True, blank=False))
+        ('feature', Features_FeatureBlock(null=True, blank=False))
     ], null=True, blank=False)
 
 
@@ -171,7 +172,7 @@ class _S_FeaturesBlock(blocks.StructBlock):
 class Steps_StepBlock(blocks.StructBlock):
     step_icon = blocks.CharBlock(null=True, blank=False, help_text="Font Awesome icon name (e.g. facebook-f) from https://fontawesome.com/icons?d=gallery&s=solid&m=free")
     step_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
-    step_image = ImageChooserBlock(null=True, blank=False, help_text="Image fitting this step")     
+    step_image = ImageChooserBlock(null=True, blank=False, help_text="Image fitting this step")
     step_paragraph = blocks.RichTextBlock(null=True, blank=False, help_text="Step paragraph", features=['bold', 'italic', 'underline', 'strikethrough', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'ol', 'ul', 'hr', 'embed', 'link', 'superscript', 'subscript', 'document-link', 'image', 'code'], classname="full")
 
 class _S_StepsBlock(blocks.StructBlock):
@@ -183,7 +184,7 @@ class _S_StepsBlock(blocks.StructBlock):
     steps_displaysubhead = blocks.BooleanBlock(null=True, blank=True, default=True, required=False, help_text="Whether or not to display the header")
     steps_button = SnippetChooserBlock(Button, null=True, blank=True, required=False, help_text="Button displayed at steps-block")
     steps_steps = blocks.StreamBlock([
-      ('step', Steps_StepBlock(null=True, blank=False))
+        ('step', Steps_StepBlock(null=True, blank=False))
     ], null=True, blank=False, max_num=4)
 
 
@@ -192,8 +193,7 @@ class _S_ManifestBlock(blocks.StructBlock):
     manifest_background = ColorBlock(null=True, blank=False, help_text="Select background color that contrasts text")
     manifest_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
     manifest_paragraph = blocks.RichTextBlock(null=True, blank=False, help_text="Manifest paragraph", features=['bold', 'italic', 'underline', 'strikethrough', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'ol', 'ul', 'hr', 'embed', 'link', 'superscript', 'subscript', 'document-link', 'image', 'code'], classname="full")
-    manifest_image = ImageChooserBlock(null=True, blank=False, help_text="Image fitting manifest-section")     
-    
+    manifest_image = ImageChooserBlock(null=True, blank=False, help_text="Image fitting manifest-section")
 
 # ## Specials Section ##
 # class Specials_SpecialBlock(blocks.StructBlock):
@@ -237,7 +237,7 @@ class _S_FacebookBlock(blocks.StructBlock):
     facebook_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
     facebook_displayhead = blocks.BooleanBlock(null=True, blank=True, default=True, required=False, help_text="Whether or not to display the header")
     facebook_urls = blocks.StreamBlock([
-      ('facebook', Facebook_PostBlock(null=True, blank=False))
+        ('facebook', Facebook_PostBlock(null=True, blank=False))
     ], null=True, blank=False, max_num=3)
 
 
@@ -251,7 +251,7 @@ class _S_InstagramBlock(blocks.StructBlock):
     instagram_displayhead = blocks.BooleanBlock(null=True, blank=True, default=True, required=False, help_text="Whether or not to display the header")
     instagram_captions = blocks.BooleanBlock(null=True, blank=True, required=False, help_text="Activate to show texts and hashtags of the given Instagram post on the website.")
     instagram_urls = blocks.StreamBlock([
-      ('instagram',Instagram_PostBlock(null=True, blank=False))
+        ('instagram',Instagram_PostBlock(null=True, blank=False))
     ], null=True, blank=False, max_num=3)
 
 
@@ -269,7 +269,7 @@ class _S_PricingBlock(blocks.StructBlock):
     pricing_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
     pricing_displayhead = blocks.BooleanBlock(null=True, blank=True, default=True, required=False, help_text="Whether or not to display the header")
     pricing_pricingcards = blocks.StreamBlock([
-      ('pricingcard', Pricing_PricingcardBlock(null=True, blank=False))
+        ('pricingcard', Pricing_PricingcardBlock(null=True, blank=False))
     ], null=True, blank=False, max_num=3)
 
 
@@ -302,91 +302,92 @@ class UniquePage(Page):
 
     copyrightholder = models.CharField(null=True, blank=False, max_length=255)
 
+    about = RichTextField(null=True, blank=False, features=['bold', 'italic', 'underline', 'strikethrough', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'ol', 'ul', 'hr', 'embed', 'link', 'superscript', 'subscript', 'document-link', 'image', 'code'])
     privacy = RichTextField(null=True, blank=False, features=['bold', 'italic', 'underline', 'strikethrough', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'ol', 'ul', 'hr', 'embed', 'link', 'superscript', 'subscript', 'document-link', 'image', 'code'])
-    
+
     sociallinks = StreamField([
-      ('link', blocks.URLBlock(help_text="Important! Format https://www.domain.tld/xyz"))
+        ('link', blocks.URLBlock(help_text="Important! Format https://www.domain.tld/xyz"))
     ])
 
     array = []
     def sociallink_company(self):
-      for link in self.sociallinks:
-        self.array.append(str(link).split(".")[1])
-      return self.array
+        for link in self.sociallinks:
+            self.array.append(str(link).split(".")[1])
+        return self.array
 
 
     headers = StreamField([
-      ('h_hero', _H_HeroBlock(null=True, blank=False, icon='image')),
-      ('code', blocks.RawHTMLBlock(null=True, blank=True, classname="full", icon='code'))
+        ('h_hero', _H_HeroBlock(null=True, blank=False, icon='image')),
+        ('code', blocks.RawHTMLBlock(null=True, blank=True, classname="full", icon='code'))
     ], null=True, blank=False)
 
     sections = StreamField([
-      ('s_why', _S_WhyBlock(null=True, blank=False, icon='group')),
-      ('s_individual', _S_IndividualBlock(null=True, blank=False, icon='user', max_num=1)),
-      ('s_experts', _S_ExpertsBlock(null=True, blank=False, icon='pick', max_num=1)),
-      ('s_lab', _S_LabBlock(null=True, blank=False, icon='snippet')),
-      ('s_method', _S_MethodBlock(null=True, blank=False, icon='site')),
-      ('s_services', _S_ServicesBlock(null=True, blank=False, icon='openquote')),
-      ('s_reviews', _S_ReviewsBlock(null=True, blank=False, icon='form')),
-      ('s_features', _S_FeaturesBlock(null=True, blank=False, icon='fa-th')),
-      ('s_steps', _S_StepsBlock(null=True, blank=False, icon='fa-list-ul')),
-      ('s_manifest', _S_ManifestBlock(null=True, blank=False, icon='fa-comments')),
-      ('s_facebook', _S_FacebookBlock(null=True, blank=False, icon='fa-facebook-official')),
-      ('s_instagram', _S_InstagramBlock(null=True, blank=False, icon='fa-instagram')),
-      ('s_pricing', _S_PricingBlock(null=True, blank=False, icon='home')),
-      ('s_about', _S_AboutBlock(null=True, blank=False, icon='fa-quote-left')),
-      ('code', blocks.RawHTMLBlock(null=True, blank=True, classname="full", icon='code'))
+        ('s_why', _S_WhyBlock(null=True, blank=False, icon='group')),
+        ('s_individual', _S_IndividualBlock(null=True, blank=False, icon='user', max_num=1)),
+        ('s_experts', _S_ExpertsBlock(null=True, blank=False, icon='pick', max_num=1)),
+        ('s_lab', _S_LabBlock(null=True, blank=False, icon='snippet')),
+        ('s_method', _S_MethodBlock(null=True, blank=False, icon='site')),
+        ('s_services', _S_ServicesBlock(null=True, blank=False, icon='openquote')),
+        ('s_reviews', _S_ReviewsBlock(null=True, blank=False, icon='form')),
+        ('s_features', _S_FeaturesBlock(null=True, blank=False, icon='fa-th')),
+        ('s_steps', _S_StepsBlock(null=True, blank=False, icon='fa-list-ul')),
+        ('s_manifest', _S_ManifestBlock(null=True, blank=False, icon='fa-comments')),
+        ('s_facebook', _S_FacebookBlock(null=True, blank=False, icon='fa-facebook-official')),
+        ('s_instagram', _S_InstagramBlock(null=True, blank=False, icon='fa-instagram')),
+        ('s_pricing', _S_PricingBlock(null=True, blank=False, icon='home')),
+        ('s_about', _S_AboutBlock(null=True, blank=False, icon='fa-quote-left')),
+        ('code', blocks.RawHTMLBlock(null=True, blank=True, classname="full", icon='code'))
     ], null=True, blank=False)
 
     token = models.CharField(null=True, blank=True, max_length=255)
 
     main_content_panels = [
-      StreamFieldPanel('headers'),
-      StreamFieldPanel('sections')
+        StreamFieldPanel('headers'),
+        StreamFieldPanel('sections')
     ]
 
     imprint_panels = [
         MultiFieldPanel(
-        [
-          FieldPanel('city'),
-          FieldPanel('zip_code'),
-          FieldPanel('address'),
-          FieldPanel('telephone'),
-          FieldPanel('telefax'),
-          FieldPanel('whatsapp_telephone'),
-          FieldPanel('whatsapp_contactline'),
-          FieldPanel('email'),
-          FieldPanel('copyrightholder')
-        ],
-        heading="contact",
-      ),
-      MultiFieldPanel(
-        [
-          FieldPanel('vat_number'),
-          FieldPanel('tax_id'),
-          FieldPanel('trade_register_number'),
-          FieldPanel('court_of_registry'),
-          FieldPanel('place_of_registry'),
-          FieldPanel('trade_register_number'),
-          FieldPanel('ownership')
-        ],
-        heading="legal",
-      ),
-      StreamFieldPanel('sociallinks'),
-      MultiFieldPanel(
-        [
-          FieldPanel('privacy')
-        ],
-        heading="privacy",
-      )
+            [
+            FieldPanel('city'),
+            FieldPanel('zip_code'),
+            FieldPanel('address'),
+            FieldPanel('telephone'),
+            FieldPanel('telefax'),
+            FieldPanel('whatsapp_telephone'),
+            FieldPanel('whatsapp_contactline'),
+            FieldPanel('email'),
+            FieldPanel('copyrightholder')
+            ],
+            heading="contact",
+        ),
+        MultiFieldPanel(
+            [
+            FieldPanel('vat_number'),
+            FieldPanel('tax_id'),
+            FieldPanel('trade_register_number'),
+            FieldPanel('court_of_registry'),
+            FieldPanel('place_of_registry'),
+            FieldPanel('trade_register_number'),
+            FieldPanel('ownership')
+            ],
+            heading="legal",
+        ),
+        StreamFieldPanel('sociallinks'),
+        MultiFieldPanel(
+            [
+            FieldPanel('privacy')
+            ],
+            heading="privacy",
+        )
     ]
 
     token_panel = [
-      FieldPanel('token')
+        FieldPanel('token')
     ]
- 
+
     edit_handler = TabbedInterface([
-      ObjectList(Page.content_panels + main_content_panels, heading='Main'),
-      ObjectList(imprint_panels, heading='Imprint'),
-      ObjectList(Page.promote_panels + token_panel + Page.settings_panels, heading='Settings', classname="settings")
+        ObjectList(Page.content_panels + main_content_panels, heading='Main'),
+        ObjectList(imprint_panels, heading='Imprint'),
+        ObjectList(Page.promote_panels + token_panel + Page.settings_panels, heading='Settings', classname="settings")
     ])
